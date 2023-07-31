e494037d67936e2efdcf1be0efdab778d11c43c1
navyanjali1904 <135079414+navyanjali1904@users.noreply.github.com>
2023-07-31 10:57:52 -0400

    let see
    experimenting rn

--- Workshop Project/Assets/Scripts/Player Scripts/PlayerEnergyPickup.cs
+++ Workshop Project/Assets/Scripts/Player Scripts/PlayerEnergyPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerEnergyPickup : MonoBehaviour
+public class PlayerEnergyPickup : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
